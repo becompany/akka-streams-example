5f4b456c522737bca52136ddde8b344a1ae107f2
Andreas Jim-Hartmann <andreas.hartmann@becompany.ch>
2016-06-24 10:20:00 +0200

    Use SourceQueue instead of ActorPublisher.

--- src/main/scala/ch/becompany/akka/io/file/FileReader.scala
+++ src/main/scala/ch/becompany/akka/io/file/FileReader.scala
@@ -1,34 +1,12 @@
 package ch.becompany.akka.io.file
 
-import java.nio.file.{Files, Path, Paths}
+import java.nio.file.Paths
 
-import akka.NotUsed
-import akka.actor.{ActorSystem, Props}
-import akka.stream.actor.ActorPublisher
-import akka.stream.scaladsl.Framing._
-import akka.stream.scaladsl.{FileIO, Source}
-import akka.stream.{ActorMaterializer, IOResult}
-import akka.util.ByteString
-
-import scala.concurrent.Future
+import akka.stream.OverflowStrategy
+import akka.stream.scaladsl.Source
+import org.apache.commons.io.input.{Tailer, TailerListenerAdapter}
 
 object FileReader {
-
-  private implicit val system = ActorSystem("akka-file-io")
-  private implicit val materializer = ActorMaterializer()
-
-  /**
-    * Reads a file.
-    *
-    * @param path The path.
-    * @param encoding The encoding.
-    * @return Either an error or the source.
-    */
-  def read(path: String, encoding: String): Source[String, Future[IOResult]] =
-    FileIO.fromPath(Paths.get(path)).
-      via(delimiter(ByteString("\n"), Int.MaxValue)).
-      map(_.decodeString(encoding))
-
 
   /**
     * Read a file continuously.
@@ -37,13 +15,14 @@ object FileReader {
     * @param encoding The encoding.
     * @return Either an error or the source.
     */
-  def readContinuously(path: String, encoding: String): Source[String, NotUsed] = {
-    val tailPublisher = system.actorOf(Props[TailPublisher])
-    val src = Source.
-      fromPublisher(ActorPublisher[ByteString](tailPublisher)).
-      map(_.decodeString(encoding))
-    new Tailer(Paths.get(path), encoding, tailPublisher).run()
-    src
-  }
+  def readContinuously[T](path: String, encoding: String): Source[String, _] =
+    Source.queue[String](bufferSize = 1000, OverflowStrategy.fail).
+      mapMaterializedValue { queue =>
+        Tailer.create(Paths.get(path).toFile, new TailerListenerAdapter {
+          override def handle(line: String): Unit = {
+            queue.offer(line)
+          }
+        })
+      }
 
 }
